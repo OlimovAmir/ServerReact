8495e6826036432d7789516566a0770ee2496c05
olimov Amir <olimov.amir@mail.ru>
2023-12-08 14:37:07 +0300

    created function create post in react

--- ClientApp/src/components/Posts.jsx
+++ ClientApp/src/components/Posts.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import styles from './Posts.module.css';
+
+const BaseUrl = `/api/posts`;
 
 function Posts() {
     const [allPost, setAllPost] = useState([]);
 
     const getPost = async () => {
         const options = {
-            method: 'GET'
+            method: 'GET',
+            headers: new Headers()
         }
-        const result = await fetch('/api/posts', options);
+        const result = await fetch(BaseUrl, options);
         if (result.ok) {
             const posts = await result.json();
             setAllPost(posts);
@@ -16,25 +20,59 @@ function Posts() {
         return [];
     }
 
-    useEffect( ()=>{
+    const AddPost = async () => {
+        const headersFromUser = document.querySelector('#header').value;
+        const textFromUser = document.querySelector('#text').value;
+
+        const newPost = {
+            header: headersFromUser,
+            text: textFromUser
+        }
+        const headers = new Headers();
+        headers.set('Content-Type', 'application/json');
+
+        const options = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(newPost)
+        }
+        const result = await fetch(BaseUrl, options);
+        if (result.ok) {
+            const post = await result.json();
+            allPost.push(post)
+            setAllPost(allPost.slice());
+        }
+    }
+
+    useEffect(() => {
         getPost();
-    }, [] )
+    }, [])
 
     return (
         <div>
             <form>
                 <p>Создание постов</p>
                 <div style={{ margin: "10px" }}>
-                    <input type="text" placeholder='Зоголовок поста' />
+                    <input id='header' type="text" placeholder='Зоголовок поста' />
                 </div>
                 <div style={{ margin: "10px" }}>
-                    <textarea />
+                    <textarea id='text' />
                 </div>
-                <button type='button' style={{ color: "green", borderRadius: "10px" }}>Add Post</button>
+                <button onClick={() => AddPost()} type='button' style={{ color: "green", borderRadius: "10px" }}>Add Post</button>
             </form>
             <div>
-            <h3>List posts</h3>
-
+                <h3>List posts</h3>
+                <div className={styles.wrapper}>
+                    {allPost.map(item => {
+                        const postView = (
+                            <div key={item.id} className={styles.wrapperItem}>
+                                <h2>{item.id} {item.header}</h2>
+                                <p> {item.text}</p>
+                            </div>
+                        );
+                        return postView;
+                    })}
+                </div>
 
             </div>
         </div>
